488831a0df58666b252cd809c3ebd2b07390a80c
Devin Schwab <digidevin@gmail.com>
2019-03-11 16:23:26 -0400

    :sparkles: Added term logging to model explorer

--- src/bin/model_explorer.rs
+++ src/bin/model_explorer.rs
@@ -3,7 +3,47 @@ use mjcf_parser::MJCFModelDesc;
 use nalgebra as na;
 use nphysics3d::world::World;
 use nphysics_testbed3d::Testbed;
+use slog;
+use slog::o;
+use slog::Drain;
+use slog_async;
+use slog_term;
 use std::fs;
+
+fn parse_level(level: &str) -> slog::Level {
+    match level.trim().to_lowercase().as_str() {
+        "trace" => slog::Level::Trace,
+        "debug" => slog::Level::Debug,
+        "info" => slog::Level::Info,
+        "warn" => slog::Level::Warning,
+        "error" => slog::Level::Error,
+        "critical" => slog::Level::Critical,
+        _ => panic!(
+            "Unknown log level {}. Must be one of [trace, debug, info, warn, error, critical]",
+            level
+        ),
+    }
+}
+
+fn make_logger(level: slog::Level, model_file: String) -> slog::Logger {
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = drain.filter_level(level).fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+
+    slog::Logger::root(
+        drain,
+        o!("model_file" => model_file,
+            "place" =>
+           slog::FnValue(move |info| {
+                       format!("{}:{} {}",
+                               info.file(),
+                               info.line(),
+                               info.module(),
+                               )
+        })),
+    )
+}
 
 fn main() {
     let matches = App::new("Model Explorer")
@@ -16,7 +56,21 @@ fn main() {
                 .required(true)
                 .index(1),
         )
+        .arg(
+            Arg::with_name("log_level")
+                .short("l")
+                .long("log-level")
+                .value_name("LEVEL")
+                .help("Set the logging level")
+                .takes_value(true),
+        )
         .get_matches();
+
+    let logger = make_logger(
+        parse_level(matches.value_of("log_level").unwrap_or("info")),
+        matches.value_of("MODEL_FILE").unwrap().to_string(),
+    );
+    mjcf_parser::set_root_logger(logger.clone());
 
     let model_xml = fs::read_to_string(matches.value_of("MODEL_FILE").unwrap())
         .expect("Failed to read model file");
@@ -38,4 +92,6 @@ fn main() {
         na::Point3::new(0.0, 0.0, 1.0),
     );
     testbed.run();
+
+    mjcf_parser::drop_root_logger();
 }
