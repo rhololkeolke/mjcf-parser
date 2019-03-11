4d7902cef48c6501634b27f1e50afd1bd9fdcae8
Devin Schwab <digidevin@gmail.com>
2019-03-11 18:46:36 -0400

    :sparkles: Added orientation parsing code

--- src/attributes/mod.rs
+++ src/attributes/mod.rs
@@ -1,4 +1,5 @@
+mod orientations;
 mod real_vector_attribute;
 
-pub use real_vector_attribute::ParseRealAttributeError;
-pub use real_vector_attribute::parse_real_vector_attribute;
+pub use orientations::{parse_orientation_attribute, ParseOrientationError};
+pub use real_vector_attribute::{parse_real_vector_attribute, ParseRealAttributeError};
